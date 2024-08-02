19d6cd9961a3cc0ac855eac9345e5effa623f3b2
mike0tdv <mihailtodev3@gmail.com>
2024-08-02 12:56:38 +0300

    update

--- app/repos/auth.py
+++ app/repos/auth.py
@@ -1,7 +1,41 @@
 from fastapi import HTTPException, status
 from repos import logged1
+import database, models, schemas
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 
+get_db = database.get_db
 
 def loggedUser():
     if logged1.logged_user == False:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Log in to see this info")
+    
+
+def loginMain(db: Session, request: OAuth2PasswordRequestForm):
+     
+    user = db.query(models.Member).filter(models.Member.name==request.username).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid login credentials")
+    
+    if not user.password == request.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password credentials")
+
+    logged1.logged_user = True
+    logged1.logged_user_name = request.username
+    return {"Data": f"Welcome back {request.username}"}
+
+
+def signIn(db: Session, request: schemas.Members):
+    
+    data = db.query(models.Member).filter(models.Member.name == request.name).first()
+    if data:
+        raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="This username is already in use")
+    new_member = models.Member(name=request.name, age=request.age, city=request.city, password=request.password, messagesSent = 0)
+    db.add(new_member)
+    db.flush()
+    db.commit()
+    db.refresh(new_member)
+    logged1.logged_user = True
+    logged1.logged_user_name = request.name
+    return new_member
